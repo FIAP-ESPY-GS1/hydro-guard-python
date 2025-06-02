1a7d2bbdacca64d7738125a59843e2bc0661c57b
Gabriel Augusto <gatop652@gmail.com>
2025-06-02 09:24:37 -0300

    feat: safe routes script

--- src/safe_routes.py
+++ src/safe_routes.py
@@ -1,9 +1,24 @@
-def find_safe_routes(distances: list[float]):
-    print(distances)
+def find_safe_routes(distances: list[float] = list()) -> list[float]:
+    aux = 0
+    for i in range(0, len(distances)):
+        for j in range(i+1, len(distances)):
+
+            if distances[i] > distances[j]:
+
+               aux = distances[i]
+               distances[i] = distances[j]
+               distances[j] = aux
+
+    if len(distances) >= 3:
+        return distances[0:3]
+    else:
+        return distances[0:len(distances)]
 
 
 def main():
-    find_safe_routes(list())
+   distances = [10.6 ,20, 30.5, 2, 40, 50]
+   result = find_safe_routes(distances)
+   print(result)
 
 
 if __name__ == "__main__":
